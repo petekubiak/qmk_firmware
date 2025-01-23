a5395d8942f80608a01b3689b951652191660e06
Pete Kubiak <peter.kubiak@dyson.com>
2025-01-23 14:47:06 +0000

    Add personal keymap

---
+++ keyboards/splitkb/kyria/keymaps/petekubiak/rules.mk
@@ -0,0 +1,6 @@
+OLED_ENABLE = yes
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGB_MATRIX_ENABLE = yes     # Disable keyboard RGB matrix, as it is enabled by default on rev3
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
+
+CONVERT_TO = liatris

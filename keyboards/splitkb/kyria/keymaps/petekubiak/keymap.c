267b9b17083dc8c7d318b9d4c3d98b2bea24bb63
Pete Kubiak <peter.kubiak@dyson.com>
2025-01-24 13:05:29 +0000

    Swap Sym and Enter

--- keyboards/splitkb/kyria/keymaps/petekubiak/keymap.c
+++ keyboards/splitkb/kyria/keymaps/petekubiak/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * |  Ctrl  |   Z  |   X  |   C  |   V  |   B  | [ {  | Nav  |  |F-keys|  ] } |   N  |   M  | ,  < | . >  | /  ? | Enter  |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |Adjust| Esc  | Nav  | Shift| Sym  |  | Enter| Space| Bksp | RGUI | RAlt |
+ *                        |Adjust| Esc  | Nav  | Shift| Enter|  | Sym  | Space| Bksp | RGUI | RAlt |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,    KC_U  ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
      KC_LSFT , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,    KC_J  ,  KC_K ,   KC_L ,KC_SCLN, UK_QUOT,
      KC_LCTL , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC, NAV   ,     _______, KC_RBRC, KC_N,    KC_M  ,KC_COMM, KC_DOT ,KC_SLSH, KC_ENT ,
-                                ADJUST , KC_ESC ,   NAV  , KC_LSFT, SYM   ,     KC_ENT , KC_SPC ,KC_BSPC, KC_RGUI,KC_RALT
+                                ADJUST , KC_ESC ,   NAV  , KC_LSFT, KC_ENT,       SYM  , KC_SPC ,KC_BSPC, KC_RGUI,KC_RALT
     ),
 
 /*

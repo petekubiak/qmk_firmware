ea551e0d83390752056d9f697a57283ca9b7e140
Pete Kubiak <peter.kubiak@dyson.com>
2025-09-24 14:41:44 +0100

    Add caps lock key for use as Compose key

--- keyboards/splitkb/kyria/keymaps/petekubiak/keymap.c
+++ keyboards/splitkb/kyria/keymaps/petekubiak/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * | LShift |   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  '  @  |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |  Ctrl  |   Z  |   X  |   C  |   V  |   B  | [ {  | Nav  |  |F-keys|  ] } |   N  |   M  | ,  < | . >  | /  ? |   =    |
+ * |  Ctrl  |   Z  |   X  |   C  |   V  |   B  | [ {  | Caps |  |F-keys|  ] } |   N  |   M  | ,  < | . >  | /  ? |   =    |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        |Adjust| Esc  | Nav  | Shift| Enter|  | Sym  | Space| Bksp | RGUI | LAlt |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,    KC_U  ,  KC_I ,   KC_O ,  KC_P , KC_DEL ,
      KC_LSFT , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,    KC_J  ,  KC_K ,   KC_L ,KC_SCLN, UK_QUOT,
-     KC_LCTL , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC, NAV   ,      FKEYS , KC_RBRC, KC_N,    KC_M  ,KC_COMM, KC_DOT ,KC_SLSH, KC_EQL ,
+     KC_LCTL , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC, KC_CAPS,     FKEYS , KC_RBRC, KC_N,    KC_M  ,KC_COMM, KC_DOT ,KC_SLSH, KC_EQL ,
                                 ADJUST , KC_ESC ,   NAV  , KC_LSFT, KC_ENT,       SYM  , KC_SPC ,KC_BSPC, KC_RGUI,KC_LALT
     ),
 
